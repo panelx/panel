4817f86d880a7858f2272f9afeda00717890b5ec
Damian Przygodzki <dprzygodzki@me.com>
2021-01-29 23:27:39 +0100

    Fix for gitignore

--- dist/start.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const server_1 = require("./services/server");
-function start() {
-    const server = new server_1.default();
-}
-exports.default = start;
-//# sourceMappingURL=start.js.map
